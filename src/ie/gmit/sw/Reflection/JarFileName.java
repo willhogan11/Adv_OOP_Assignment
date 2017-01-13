ffde9e0e3e8196eaf5865ad6bf8f611de42756a5
Will Hogan <willhogan11@hotmail.com>
2017-01-13 23:44:58 +0000

    Created UML Diagram

--- src/ie/gmit/sw/Reflection/JarFileName.java
+++ src/ie/gmit/sw/Reflection/JarFileName.java
@@ -2,6 +2,13 @@ package ie.gmit.sw.Reflection;
 
 /* Used to Fully Encapsulate the jar file name. 
  * No one can access this and could even be located off-site*/
+
+/**
+ * Used to Fully Encapsulate the jar file name. 
+ * No one can access this and could even be located off-site
+ * @author Will Hogan
+ * @category Advanced Object oriented Programming
+ */
 public class JarFileName {
 
 	private String jarFileName = "string-service.jar";
