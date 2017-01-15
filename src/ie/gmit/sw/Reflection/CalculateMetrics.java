7a059c781b308ca9cea6544838818161d1923f96
Will Hogan <willhogan11@hotmail.com>
2017-01-15 17:07:06 +0000

    Completed all java-docs for all classes in the Reflection package

--- src/ie/gmit/sw/Reflection/CalculateMetrics.java
+++ src/ie/gmit/sw/Reflection/CalculateMetrics.java
@@ -6,7 +6,12 @@ import java.util.Map.Entry;
 
 
 /**
- * 
+ * A class that is used to calculate the Afferent and Efferent couplings and Stability of a given class / jar file.
+ * For each class name in the List object returned from the {@link ReadJarFileData} class, 
+ * add to the Key section of a Map and create a new Metric Object as the value (See the populateMap method in this class). 
+ * As the Metrics are calculated, each Metric object in the map is updated with the Class Name, In Degree, Out Degree and Stability. 
+ * To calculate the metrics for each class, Interfaces, Constructors, Fields and Methods are analysed and the data added to the HashMap. 
+ * The calculateMetrics() class returns the 2d Object Array that's then used bu the <b>View</b> or GUI section of the Application. 
  * 
  * @author Will Hogan
  * @category Advanced Object oriented Programming
@@ -20,9 +25,19 @@ public class CalculateMetrics implements Metricable {
 	private Class<?> cls;
 	private ReadJarFileData readJarFileData;
 	
-	
+	/**
+	 * For each class name in the List object returned from the {@link ReadJarFileData} class, 
+	 * add to the Key section of a Map and create a new Metric Object as the value (See the populateMap method in this class). 
+	 * As the Metrics are calculated, each Metric object in the map is updated with the Class Name, In Degree, Out Degree and Stability. 
+	 * To calculate the metrics for each class, Interfaces, Constructors, Fields and Methods are analysed and the data added to the HashMap. 
+	 * The calculateMetrics() class returns the 2d Object Array that's then used bu the <b>View</b> or GUI section of the Application. 
+	 * 
+	 * @return arrayObject
+	 * @throws ClassNotFoundException
+	 */
 	public Object[][] calculateMetrics() throws ClassNotFoundException {
 	
+		// Initialising private instance, and local variables  
 		listOfClasses = new ListClass();
 		metricMap = new HashMap<String, Metric>();
 		metricFactory = MetricFactory.getInstance();
@@ -33,20 +48,25 @@ public class CalculateMetrics implements Metricable {
 		// Populate a hashmap, with a filled list of Class Names and Metric Objects
 		metricMap = populateMap();
 		
+		// Create new ObjectMapAdapter and 2d Object Array objects 
 		ObjectMapAdapter objectMapAdapter;
 		Object[][] arrayObject;
 		
 		// Display the Names of each class name associated with the 'cls' instance
 		for (int i = 0; i < metricMap.size(); i++) {
-
+			
+			// For each class name in the list, dynamically load Classes (by name) through invoking the Java class loader
 			cls = Class.forName(listOfClasses.retrieve(i));
 			
-			// Interfaces
+			// Interfaces:
 			Class[] interfaces = cls.getInterfaces();
 			
+			// For each Class that's an Interface....
 			for (Class c : interfaces) {
 				
 				// Ignore anything outside of this package
+				// Add the name to the metric name object for that iteration
+				// Increment both the out and in degrees
 				if(metricMap.containsKey(c.getName())) {
 					
 					metric = metricMap.get(c.getName());
@@ -57,7 +77,9 @@ public class CalculateMetrics implements Metricable {
 				}
 			}
 			
-			// Constructors
+			// Continue with the same structure for each Constructor, Field and Method of that class [See Below] 
+			
+			// Constructors: 
 			Constructor ctorlist[] = cls.getDeclaredConstructors();
 			
 			for (Constructor constructor : ctorlist) {
@@ -108,15 +130,32 @@ public class CalculateMetrics implements Metricable {
 					
 					outDegree = metric.getOutDegree();
 				}
+				
+				methodParams = method.getParameterTypes();
+				
+				for (Class mParameters : methodParams) {
+					
+					if(metricMap.containsKey(mParameters.getName())) {
+						
+						metric = metricMap.get(mParameters.getName());
+						metric.setOutDegree(metric.getOutDegree() + 1);
+						metric.setInDegree(metric.getInDegree() + 1);
+						
+						outDegree = metric.getOutDegree();
+					}
+				}
 			}
 			
+			// For the class being analysed, get the name of the class and set the incremented out degree
 			metricMap.get(cls.getName()).setOutDegree(outDegree);
 			
 		} //  End metricMap.size()	
 		
+		// Initialise a new Instance from the ObjectMapAdapter class and assign a new 2d Object arrya with it's returned values. 
 		objectMapAdapter = new ObjectMapAdapter();
 		arrayObject = objectMapAdapter.adapt(metricMap, metric);
 		
+		// Return the 2d Object Array
 		return arrayObject;		
 	}
 	
--- src/ie/gmit/sw/Reflection/Metricable.java
+++ src/ie/gmit/sw/Reflection/Metricable.java
@@ -11,7 +11,31 @@ import java.util.Map;
  */
 public interface Metricable {
 
+	/**
+	 * For each class name in the List object returned from the {@link ReadJarFileData} class, 
+	 * add to the Key section of a Map and create a new Metric Object as the value (See the populateMap method in this class). 
+	 * As the Metrics are calculated, each Metric object in the map is updated with the Class Name, In Degree, Out Degree and Stability. 
+	 * To calculate the metrics for each class, Interfaces, Constructors, Fields and Methods are analysed and the data added to the HashMap. 
+	 * The calculateMetrics() class returns the 2d Object Array that's then used bu the <b>View</b> or GUI section of the Application. 
+	 * 
+	 * @return arrayObject
+	 * @throws ClassNotFoundException
+	 */
 	public abstract Object[][] calculateMetrics() throws ClassNotFoundException;
+	
+	/**
+	 * Display Values stored in the Map, <String, Metric>
+	 * This displays the stored values for the Key(Class Names) and Metric Object(ClassName, InDegree, OutDegree, Stability)
+	 * 
+	 * @param metricMap
+	 */
 	public abstract void displayMapValues(Map<String, Metric> metricMap);
+	
+	/**
+	 * A method that returns a Map. The map uses the returned values from the ListClass object, 
+	 * to populate the Key of the map and a new Metric Object is placed as the value for each class Name in the list. 
+	 * 
+	 * @return metricMap
+	 */
 	public abstract Map<String, Metric> populateMap();
 }
